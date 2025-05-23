f76fb5a5237c36118a6c8a85c25f5964bc872f96
Josh Kolenko <jkolenko+github@proton.me>
2025-05-23 16:31:28 -0400

    feat: intro, styling & images

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,24 +3,26 @@ import ReactCountdown from 'react-countdown';
 function DateSegment({ value, label }: { value: number; label: string }) {
   return (
     <div className="flex flex-col items-center">
-      <span className="text-2xl md:text-3xl">{value}</span>
-      <span className="md:text-xl">{label}</span>
+      <span className="text-2xl md:text-4xl leading-4 md:leading-6">{value}</span>
+      <span className="text-sm md:text-lg">{label}</span>
     </div>
   );
 }
 
 export default function Countdown({ date }: { date: Date }) {
   return (
-    <ReactCountdown
-      date={date}
-      renderer={({ days, hours, minutes, seconds }) => (
-        <p className="font-body flex gap-8 md:gap-14">
-          <DateSegment value={days} label="days" />
-          <DateSegment value={hours} label="hours" />
-          <DateSegment value={minutes} label="minutes" />
-          <DateSegment value={seconds} label="seconds" />
-        </p>
-      )}
-    />
+    <div className="bg-black text-white px-5 py-1.5 md:px-8 md:py-2.5">
+      <ReactCountdown
+        date={date}
+        renderer={({ days, hours, minutes, seconds }) => (
+          <p className="font-body flex gap-6 md:gap-8 relative top-0.5">
+            <DateSegment value={days} label="days" />
+            <DateSegment value={hours} label="hours" />
+            <DateSegment value={minutes} label="minutes" />
+            <DateSegment value={seconds} label="seconds" />
+          </p>
+        )}
+      />
+    </div>
   );
 }
