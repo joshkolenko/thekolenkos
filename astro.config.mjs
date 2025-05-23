e8a764ac1bcf8fb2a8b5f605ff76e5f9fae4453c
Josh Kolenko <jkolenko+github@proton.me>
2025-05-23 02:48:45 -0400

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import clerk from '@clerk/astro';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [clerk(), react({ experimentalReactChildren: true })],
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   output: 'server',
   vite: {
     plugins: [tailwindcss()],
