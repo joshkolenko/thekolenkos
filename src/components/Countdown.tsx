139f78d5e4e9132dbd730b10f20c46c63a08aff0
Josh Kolenko <jkolenko+github@proton.me>
2025-05-23 02:41:28 -0400

    feat: mobile styles

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,15 +1,24 @@
 import ReactCountdown from 'react-countdown';
 
+function DateSegment({ value, label }: { value: number; label: string }) {
+  return (
+    <div className="flex flex-col items-center">
+      <span className="text-2xl md:text-3xl">{value}</span>
+      <span className="md:text-xl">{label}</span>
+    </div>
+  );
+}
+
 export default function Countdown({ date }: { date: Date }) {
   return (
     <ReactCountdown
       date={date}
       renderer={({ days, hours, minutes, seconds }) => (
-        <p className="text-2xl font-body flex gap-6">
-          <span>{days} days</span>
-          <span>{hours} hours</span>
-          <span>{minutes} minutes</span>
-          <span>{seconds} seconds</span>
+        <p className="font-body flex gap-8 md:gap-14">
+          <DateSegment value={days} label="days" />
+          <DateSegment value={hours} label="hours" />
+          <DateSegment value={minutes} label="minutes" />
+          <DateSegment value={seconds} label="seconds" />
         </p>
       )}
     />
