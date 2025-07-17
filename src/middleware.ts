a808ca8f05ad550f58f3b96139eb3dd8d2e2a4e0
Josh Kolenko <jkolenko+github@proton.me>
2025-07-17 00:42:12 -0400

    test: remove middleware for deployment test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,44 @@
-import { defineMiddleware } from 'astro:middleware';
-import * as cookie from 'cookie';
-import { config } from 'dotenv';
+// import { defineMiddleware } from 'astro:middleware';
+// import * as cookie from 'cookie';
+// import { config } from 'dotenv';
 
-config();
+// config();
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const ignoredPaths = ['/api/code', '/api/submit', '/.well-known', '/favicon.ico'];
-  const cookies = cookie.parse(context.request.headers.get('cookie') || '');
+// export const onRequest = defineMiddleware(async (context, next) => {
+//   const ignoredPaths = ['/api/code', '/api/submit', '/.well-known', '/favicon.ico'];
+//   const cookies = cookie.parse(context.request.headers.get('cookie') || '');
 
-  if (
-    context.url.pathname === '/code' ||
-    ignoredPaths.some(path => context.url.pathname.startsWith(path)) ||
-    (cookies.code && cookies.code === process.env.ACCESS_CODE)
-  ) {
-    return next();
-  }
+//   if (
+//     context.url.pathname === '/code' ||
+//     ignoredPaths.some(path => context.url.pathname.startsWith(path)) ||
+//     (cookies.code && cookies.code === process.env.ACCESS_CODE)
+//   ) {
+//     return next();
+//   }
 
-  const codeParam = context.url.searchParams.get('code');
+//   const codeParam = context.url.searchParams.get('code');
 
-  if (codeParam && codeParam === process.env.ACCESS_CODE) {
-    const response = await next();
+//   if (codeParam && codeParam === process.env.ACCESS_CODE) {
+//     const response = await next();
 
-    response.headers.set(
-      'Set-Cookie',
-      cookie.serialize('code', codeParam, {
-        path: '/',
-        httpOnly: true,
-        // secure: true,
-        sameSite: 'strict',
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 year
-      })
-    );
+//     response.headers.set(
+//       'Set-Cookie',
+//       cookie.serialize('code', codeParam, {
+//         path: '/',
+//         httpOnly: true,
+//         // secure: true,
+//         sameSite: 'strict',
+//         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 year
+//       })
+//     );
 
-    return response;
-  }
+//     return response;
+//   }
 
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: '/code',
-    },
-  });
-});
+//   return new Response(null, {
+//     status: 302,
+//     headers: {
+//       Location: '/code',
+//     },
+//   });
+// });
